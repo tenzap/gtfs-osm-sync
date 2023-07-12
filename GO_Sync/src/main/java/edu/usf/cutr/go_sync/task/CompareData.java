67f565baa16fc89573265f126514cea66303a0cf
tenzap <fabstz-it@yahoo.fr>
2023-07-12 22:52:43 +0200

    PERSONNAL: when at the same position, only consider equal when both are platform (to give priority to plaform and not station for the same location)

--- GO_Sync/src/main/java/edu/usf/cutr/go_sync/task/CompareData.java
+++ GO_Sync/src/main/java/edu/usf/cutr/go_sync/task/CompareData.java
@@ -653,6 +653,7 @@ private ArrayList<Hashtable> OSMRelationTags = new ArrayList<Hashtable>();
             osmtag.putAll(OSMTags.get(osmindex));
             String osmOperator = (String)osmtag.get(tag_defs.OSM_NETWORK_KEY);
             String osmStopID = (String)osmtag.get("gtfs_id");
+            String osmPlatformType = (String)osmtag.get(tag_defs.OSM_STOP_TYPE_KEY);
             //add leading 0's
             if(osmStopID!=null) {
                 if (!osmStopID.equals("missing")) {
@@ -837,6 +838,7 @@ private ArrayList<Hashtable> OSMRelationTags = new ArrayList<Hashtable>();
                             }
                             // if same lat and lon --> possible same exact stop --> add gtfs_id, operator, stop_name
                             else {
+                              if (osmPlatformType.equals(gtfsStop.getOsmPublicTransportType())) {
                                 Stop ns = new Stop(gtfsStop);
                                 ns.addTags(osmtag);
                                 ns.setOsmId(node.getValue("id"));
@@ -861,6 +863,9 @@ private ArrayList<Hashtable> OSMRelationTags = new ArrayList<Hashtable>();
                                 ns.setReportCategory("MODIFY");
                                 addToReport(ns, es, true);
                                 break;
+                              } else {
+                                  // TODO
+                              }
                             }
                         }
                     }
