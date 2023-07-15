a2d8471149ac1ead5fc857ece91b301ba12b3d1b
tenzap <fabstz-it@yahoo.fr>
2023-07-15 18:04:28 +0200

    convert HashSet to LinkedHashSet for relation members
    Because order is important in ptv2

--- GO_Sync/src/main/java/edu/usf/cutr/go_sync/osm/HttpRequest.java
+++ GO_Sync/src/main/java/edu/usf/cutr/go_sync/osm/HttpRequest.java
@@ -22,6 +22,7 @@ import java.util.ArrayList;
 
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.LinkedHashSet;
 import java.util.List;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
@@ -69,7 +70,7 @@ public class HttpRequest {
     private ArrayList<AttributesImpl> existingRelations = new ArrayList<AttributesImpl>();
     private ArrayList<Hashtable> existingBusTags = new ArrayList<Hashtable>();
     private ArrayList<Hashtable> existingRelationTags = new ArrayList<Hashtable>();
-    private ArrayList<HashSet<RelationMember>> existingRelationMembers = new ArrayList<HashSet<RelationMember>>();
+    private ArrayList<LinkedHashSet <RelationMember>> existingRelationMembers = new ArrayList<LinkedHashSet <RelationMember>>();
 
     private HashSet<Stop> revertDelete = new HashSet<Stop>();
     private HashSet<Stop> revertModify = new HashSet<Stop>();
@@ -233,7 +234,7 @@ public class HttpRequest {
     }
 
     // this method needs to be invoked after getExistingBusRelations
-    public ArrayList<HashSet<RelationMember>> getExistingBusRelationMembers(){
+    public ArrayList<LinkedHashSet <RelationMember>> getExistingBusRelationMembers(){
         System.out.println("tags = "+existingRelationMembers.size());
         if (!existingRelationMembers.isEmpty() )
             return existingRelationMembers;
