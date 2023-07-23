4bc4f294787d6a1c6817762f6c2a4fc8892e00dd
tenzap <fabstz-it@yahoo.fr>
2023-07-23 15:03:45 +0200

    add ProcessingOptions enum (empty for now)

---
+++ GO_Sync/src/main/java/edu/usf/cutr/go_sync/object/ProcessingOptions.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package edu.usf.cutr.go_sync.object;
+
+/**
+ *
+ * @author tenzap
+ */
+public enum ProcessingOptions {
+    // General
+
+    // For Stops
+
+    // For Routes
+
+    // For Route Members
+
+}
