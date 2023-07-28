749d98cc7b904959ded27fc3568341784502b2be
tenzap <fabstz-it@yahoo.fr>
2023-07-28 17:13:28 +0200

    Revert "PERSONNAL: when at the same position, only consider equal when both are platform (to give priority to plaform and not station for the same location)"
    This reverts commit 67f565baa16fc89573265f126514cea66303a0cf.

--- GO_Sync/src/main/java/edu/usf/cutr/go_sync/task/CompareData.java
+++ GO_Sync/src/main/java/edu/usf/cutr/go_sync/task/CompareData.java
@@ -1014,7 +1014,6 @@ private ArrayList<Hashtable> OSMRelationTags = new ArrayList<Hashtable>();
             osmtag.putAll(OSMTags.get(osmindex));
             String osmOperator = (String)osmtag.get(tag_defs.OSM_NETWORK_KEY);
             String osmStopID = (String)osmtag.get("gtfs_id");
-            String osmPlatformType = (String)osmtag.get(tag_defs.OSM_STOP_TYPE_KEY);
             //add leading 0's
             if(osmStopID!=null) {
                 if (!osmStopID.equals("missing")) {
@@ -1205,7 +1204,6 @@ private ArrayList<Hashtable> OSMRelationTags = new ArrayList<Hashtable>();
                             }
                             // if same lat and lon --> possible same exact stop --> add gtfs_id, operator, stop_name
                             else {
-                              if (osmPlatformType.equals(gtfsStop.getOsmPublicTransportType())) {
                                 Stop ns = new Stop(gtfsStop);
                                 ns.addTags(osmtag);
                                 ns.setOsmId(node.getValue("id"));
@@ -1230,9 +1228,6 @@ private ArrayList<Hashtable> OSMRelationTags = new ArrayList<Hashtable>();
                                 ns.setReportCategory("MODIFY");
                                 addToReport(ns, es, true);
                                 break;
-                              } else {
-                                  // TODO
-                              }
                             }
                         }
                     }
