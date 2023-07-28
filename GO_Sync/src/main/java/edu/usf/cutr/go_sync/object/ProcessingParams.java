3fcefc8f9bb8f726ec291594c1c581448f5be3c6
tenzap <fabstz-it@yahoo.fr>
2023-07-28 16:20:09 +0200

    Stops & Netex: don't prefix station name with TOWN for some cities - part 2 (UI)

---
+++ GO_Sync/src/main/java/edu/usf/cutr/go_sync/object/ProcessingParams.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.usf.cutr.go_sync.object;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tenzap
+ */
+public class ProcessingParams {
+    ArrayList<String> stopCities;
+
+    public ArrayList<String> getStopCities() {
+        return stopCities;
+    }
+
+    public void setStopCities(ArrayList<String> stopCities) {
+        this.stopCities = stopCities;
+    }
+}
